5728b7e61d6f8d3a317507bd83d29f1d846b1654
gaoshiqi <gaoshiqi@bilibili.com>
2025-06-27 21:42:56 +0800

    新建map模块

--- map/build.gradle.kts
+++ map/build.gradle.kts
@@ -1,24 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.example.bangumi"
+    namespace = "com.example.map"
     compileSdk = 35
 
-    buildFeatures {
-        viewBinding = true
-    }
-
     defaultConfig {
-        applicationId = "com.example.bangumi"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -42,23 +35,9 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.viewmodel.ktx)
-    implementation(libs.liveData.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    // Retrofit
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.gson)
-
-    // glide
-    implementation(libs.glide)
-
-    // map
-    implementation(project(":map"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Bangumi"
 include(":app")
+include(":map")
