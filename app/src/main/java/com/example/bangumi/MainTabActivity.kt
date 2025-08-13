6b63789294dc394d253498ad4f3cf6ad554f4b21
gaoshiqi <gaoshiqi@bilibili.com>
2025-08-13 22:27:10 +0800

    FIX-修复新首页顶部间距

--- app/src/main/java/com/example/bangumi/MainTabActivity.kt
+++ app/src/main/java/com/example/bangumi/MainTabActivity.kt
@@ -2,6 +2,8 @@ package com.example.bangumi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.bangumi.databinding.ActivityMainTabBinding
@@ -26,6 +28,12 @@ class MainTabActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainTabBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         
         setupBottomNavigation()
         
